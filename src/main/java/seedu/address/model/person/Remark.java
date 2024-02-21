489c32163c895a45c497b65dcca207fce8c98f99
Zhi Heng <tanzhiheng26@gmail.com>
2024-02-21 11:06:33 +0800

    Add Remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,46 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+public class Remark {
+    public final String value;
+
+    /**
+     * Constructs an {@code remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherremark = (Remark) other;
+        return value.equals(otherremark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
